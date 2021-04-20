fc4a0b946c79df67ff7ef550c69606cc041994e1
William <william.bertrand94@gmail.com>
2021-04-19 17:35:22 -0700

    Initial commit

---
+++ Assets/MenuResources/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+        // TODO: If your game does not have GameScene as the scene directly after the menu scene, change the next line
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# unity-starter-2d
+ Starter project for unity 2D, really only contains a basic menu 
